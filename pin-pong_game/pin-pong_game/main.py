1c9a2772d88ed14a3735f4dddde1b96e490b9b40
ffffaaf <151867710+ffffaaf@users.noreply.github.com>
2023-11-24 20:45:40 +0300

    V2.0
    Добавлен мяч и отскок мяча, также добавлено проигрыш одного из игроков

--- pin-pong_game/pin-pong_game/main.py
+++ pin-pong_game/pin-pong_game/main.py
@@ -6,6 +6,8 @@ font.init()
 window_wight = 700
 window_height = 500
 font = font.SysFont('Arial' , 40)
+text_lose_player_1 = font.render('Первый игрок проиграл!' , True , (180 , 0 , 0))
+text_lose_player_2 = font.render('Второй игрок проиграл!' , True , (180 , 0 , 0))
 
 window = display.set_mode((window_wight , window_height))
 display.set_caption('Pin-Pong')
@@ -14,7 +16,7 @@ background = transform.scale(image.load('background.jpg') , (window_wight , wind
 
 game = True
 clock = time.Clock()
-FPS = 40
+FPS = 90
 
 class GameSprite(sprite.Sprite):
     def __init__(self , player_x , player_y , player_image , player_speed , size_x , size_y):
@@ -43,28 +45,40 @@ class Player(GameSprite):
         if keys_pressed[K_s] and self.rect.y < 395:
             self.rect.y += self.speed
 
-class Enemy(GameSprite):
-    def update(self):
-        self.rect.y += self.speed
-        global lost
-        if self.rect.y  > window_height:
-            self.rect.x = randint(80 , window_wight - 80)
-            self.rect.y = - 50
-            lost = lost + 1
+ball = GameSprite(0 , 0 , 'tenis_ball.png' , 4 , 50 , 50)
+rocket_1 = Player(0 , 245 , 'racket.png' , 4 , 30 , 100)
+rocket_2 = Player(670 , 245 , 'racket.png' , 4 , 30 , 100)
 
-rocket_1 = Player(0 , 245 , 'racket.png' , 3 , 30 , 100)
-rocket_2 = Player(670 , 245 , 'racket.png' , 3 , 30 , 100)
+speed_x = 3
+speed_y = 3
+finish = False
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        window.blit(background , (0 , 0))
+        rocket_1.update_L()
+        rocket_1.reset()
+        rocket_2.update_R()
+        rocket_2.reset()
+        ball.reset()
+        if ball.rect.y > window_height -50 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(rocket_1 , ball) or sprite.collide_rect(rocket_2 , ball):
+            speed_x *= -1
+        if ball.rect.x < 0:
+            window.blit(text_lose_player_1 , (175 , 200))
+            finish = True
+        if ball.rect.x > 700:
+            window.blit(text_lose_player_2 , (175 , 200))
+            finish = True
+           
 
-    window.blit(background , (0 , 0))
-    rocket_1.update_L()
-    rocket_1.reset()
-    rocket_2.update_R()
-    rocket_2.reset()
-    
-    display.update()
     clock.tick(FPS)
+    display.update()
+
+        
